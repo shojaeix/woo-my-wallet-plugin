418519740d6a18c260564b7bd3f4ee4d507b9e1b
shojaei.x <shojaei.x@gmail.com>
2018-12-12 14:10:35 +0330

    define activation functions

--- woo-my-wallet-plugin.php
+++ woo-my-wallet-plugin.php
@@ -5,3 +5,19 @@ Description:  Add wallet to woocommerce users with basic abilities
 */
 
 require_once 'vendor/autoload.php';
+
+// Activation
+register_activation_hook( __FILE__, 'wMyWallet_activation' );
+function wMyWallet_activation(){
+
+    // todo | create wallet transactions table if not exists
+
+}
+
+// Deactivation
+register_deactivation_hook(__FILE__, 'wMyWallet_deactivation');
+function wMyWallet_deactivation(){
+    // todo | Remove wallet transactions table if user confirmed this
+}
+
+
