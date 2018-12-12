4e48431ef05ed86f8cb5dba5477334a8d344b69d
shojaei.x <shojaei.x@gmail.com>
2018-12-12 16:18:22 +0330

    create transactions table on activation

--- woo-my-wallet-plugin.php
+++ woo-my-wallet-plugin.php
@@ -5,12 +5,37 @@ Description:  Add wallet to woocommerce users with basic abilities
 */
 
 require_once 'vendor/autoload.php';
+// load include directory files
+$include_files = scandir( __DIR__ . '/include' );
+if( count($include_files)){
+    foreach($include_files as $include_file ){
+        if( $include_file == '.' || $include_file == '..' ) continue;
+        if( is_file(__DIR__ . '/include/' . $include_file) )
+            require_once(__DIR__ . '/include/' . $include_file);
+    }
+}
 
 // Activation
 register_activation_hook( __FILE__, 'wMyWallet_activation' );
 function wMyWallet_activation(){
 
-    // todo | create wallet transactions table if not exists
+    // create wallet transactions table if not exists
+    $transactions_table_name = $wpdb->prefix . "wMyWallet_transactions";
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE 
+    {$table_name1} 
+    (  `id` INT NOT NULL AUTO_INCREMENT ,
+       `amount` INT UNSIGNED NOT NULL ,
+       `type` VARCHAR(20) NOT NULL ,
+       `created_at` DATETIME NOT NULL ,
+       `user_id` INT UNSIGNED NOT NULL ,
+       `old_amount` INT UNSIGNED NOT NULL ,
+       `new_amount` INT UNSIGNED NOT NULL ,
+        PRIMARY KEY (`id`)
+    ) $charset_collate;";
+
+    wMyWallet_DBHelper::dbDelta($sql);
 
 }
 
