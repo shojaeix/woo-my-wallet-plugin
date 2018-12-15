9b46ed0c8fc72734b7f8ee704f7a39a5e8bce4fb
shojaei.x <shojaei.x@gmail.com>
2018-12-15 10:37:20 +0330

    add insert_new_transaction function

--- include/plugin_functions.php
+++ include/plugin_functions.php
@@ -3,7 +3,32 @@
 // ckeck for duplicate
 if(!isset($wMyWallet_functions_loaded) or !$wMyWallet_functions_loaded){
 
-    function wMyWallet_insert_new_transaction(){}
+    function wMyWallet_insert_new_transaction($user_id,$amount,$type,$old_amount,$new_amount,$created_at = null){
+        // create created_at if is null
+        if($created_at == null){
+            $datetime = new DateTime('now',new DateTimeZone('Asia/Tehran'));
+            $created_at = wMyWallet_datetime_to_string($datetime);
+        }
+        // convert created at to string if it's Object
+        else if ($created_at instanceof DateTime){
+            $created_at = wMyWallet_datetime_to_string($created_at);
+        }
+        // validate type
+        if(!in_array($type, ['debptor', 'creditor'])){
+            throw new Exception('invalid type');
+        }
+
+        $data = [
+            'user_id' => $user_id,
+            'amount' => $amount,
+            'type' => $type,
+            'old_amount' => $old_amount,
+            'new_amount' => $new_amount,
+            'created_at' => $created_at,
+        ];
+
+        wMyWallet_DBHelper::insert('transactions',$data);
+    }
 
     function wMyWallet_get_wallet_transactions(){}
 
