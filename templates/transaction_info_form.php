887dc881545631b874280f47293904724341e919
shojaei.x <shojaei.x@gmail.com>
2018-12-18 09:23:49 +0330

    add new transaction and transaction info page

---
+++ templates/transaction_info_form.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Almasvareh
+ * Date: 12/17/2018
+ * Time: 16:58
+ */
+?>
+<form method="get">
+    <input type="hidden" name="page"  value="wmywallet-transaction-info">
+    شناسه تراکنش<input name="transaction_id" value="<?php echo isset($_GET['transaction_id']) ? $_GET['transaction_id'] : '' ?>" type="number">
+    <br>
+    <button class="button-primary" type="submit">مشاهده </button>
+</form>
