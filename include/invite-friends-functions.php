d219c28edc2a26cdce17d3d95cf1be0047b00a36
shojaeix <shojaei.x@gmail.com>
2019-02-21 10:30:55 +0330

    add comments

---
+++ include/invite-friends-functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amin
+ * Date: 2/21/2019
+ * Time: 10:06 AM
+ */
+
+//++++ Short code
+
+//---- end short code
+
+//++++ Function sendmail
+
+//---- end function
+
+//++++ GEt invite link: function
+
+//--- end get invite link
